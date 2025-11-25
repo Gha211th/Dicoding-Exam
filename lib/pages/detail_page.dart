82ed77e28222e00a71822bad27e9bc3af1a32cf8
ghatraxion <agh.arrafif@gmail.com>
2025-11-25 14:15:59 +0700

    done

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/pokemon.dart';
+import 'package:flutter_application_1/widgets/type_chip.dart';
 
 class pokemonDetailPage extends StatefulWidget {
   final Pokemon pokemon;
@@ -48,11 +49,102 @@ class _pokemonDetailPageState extends State<pokemonDetailPage> {
             ),
           ],
         ),
-        body: Column(
-          children: [
-            Center(child: Image.asset(p.image)),
-            Text(p.name),
-          ],
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            // variable layar lebar
+            final _isWide = constraints.maxWidth >= 700;
+
+            // widget imagebox
+            final imageBox = Hero(
+              tag: p.name,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  p.image,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+
+            // widget infobox
+            final infoBox = Card(
+              margin: EdgeInsets.all(15.0),
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      p.name.toUpperCase(),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    PokemonTypeChip(type: p.type),
+                    const SizedBox(height: 10),
+                    Text(
+                      "Base Power: ${p.basePower}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(height: 10, width: 10),
+                    Text(
+                      "Deskripsi",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5, width: 10),
+                    Text(p.description, textAlign: TextAlign.justify),
+                    const SizedBox(height: 10, width: 10),
+                    Text(
+                      "skills",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5, width: 10),
+                    Wrap(
+                      spacing: 8.0,
+                      children: p.skills.map((skills) {
+                        return Chip(
+                          label: Text(skills),
+                          avatar: Icon(Icons.star),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+            );
+
+            if (_isWide) {
+              return Row(
+                children: <Widget>[
+                  Expanded(flex: 1, child: imageBox),
+                  Expanded(flex: 1, child: infoBox),
+                ],
+              );
+            }
+
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsetsGeometry.all(12.0),
+                    child: imageBox,
+                  ),
+                  infoBox,
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
